d865bf1f4f4859ebbb38b7f21099c4ec720cb42d
Jose Barroso Galindo <jose.barroso-galindo@kolibrigames.com>
2020-10-08 19:28:16 +0200

    First commit.

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            Assert.That(true, Is.EqualTo(true));
+        }
+    }
+}
